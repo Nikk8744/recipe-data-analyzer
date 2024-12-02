260656c6c5fb57df901d0c986a9116df0e6a1bc2
Nikk8744 <nikk8744@gmail.com>
2024-12-02 23:51:55 +0530

    fetch api

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 const axios = require("axios");
+const collect = require("collect.js");
+require('dotenv').config();
+
+const API_URL = "https://api.spoonacular.com/recipes/complexSearch";
+
+
+async function fetchRecipes(){
+    try {
+        const response = await axios.get(API_URL, {
+            params: {
+                "apiKey": process.env.API_KEY,
+                "number": 10,
+                "addRecipeInformation": true
+            }
+        });
+        return response.data.results;
+    } catch (error) {
+        console.log(error)
+    }
+}
+// method - 1
+ async function displayRecipes(){
+    const recipes = await fetchRecipes();
+    console.log(recipes[1].title)
+    console.log(recipes)
+ }
+
+//  another method - 2
+ fetchRecipes().then(recipes => console.log(recipes))
+.catch(error => console.error(error));
+
+ displayRecipes();
